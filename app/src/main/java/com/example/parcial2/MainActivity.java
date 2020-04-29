eafcb1c981bb92d99a1f35eb4c9e9710f69d4079
Miguel Barro <miguelbarroe@gmail.com>
2020-04-29 05:31:44 -0700

    Fragment Added

--- app/src/main/java/com/example/parcial2/MainActivity.java
+++ app/src/main/java/com/example/parcial2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.parcial2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.clContainer, new RecyclerFragment());
+        ft.commit();
     }
 }
